31aeda6ba69a81694046e7742310d7a1d6e2b1d5
YuvrajM108 <yuvrajmanoo1@gmail.com>
2021-04-22 13:39:17 -0400

    Implement bubble-sort-by method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -15,3 +15,29 @@ def bubble_sort(n_arr)
   end
   n_arr
 end
+
+array_of_nos = [7, 15, 1, 2, 19, 7, 25, 6]
+puts bubble_sort(array_of_nos).join(', ')
+
+def bubble_sort_by(s_arr)
+  k = 0
+  while k < (s_arr.length - 1)
+    l = 0
+    while l < (s_arr.length - 1)
+      idx_1_word = s_arr[l]
+      idx_2_word = s_arr[l + 1]
+      result = yield(idx_1_word, idx_2_word)
+      if result > 0
+        s_arr[l] = idx_2_word
+        s_arr[l + 1] = idx_1_word
+      end
+      l += 1
+    end
+    k += 1
+  end
+  puts s_arr.join(', ')
+end
+
+bubble_sort_by(["hi", "hello", "hey"]) do |left, right|
+  left.length <=> right.length
+end
